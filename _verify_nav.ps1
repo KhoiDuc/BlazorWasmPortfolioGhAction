823776fc4448dd8dc377078b7af30f01129cc482
KhoiDuc <40898660+war-man@users.noreply.github.com>
2026-09-03 17:34:53 +0700

    chore: Sync Portfolio, MainLayout, app.css, app.js; remove temp verify scripts

--- _verify_nav.ps1
+++
@@ -1,5 +0,0 @@
-$en = [System.IO.File]::ReadAllText('d:\BlazorWasmPortfolioGhAction\Resources\SharedResources.en.resx', [System.Text.Encoding]::UTF8)
-$i = $en.IndexOf('Nav_LoadingPage')
-Write-Host "Index: $i"
-Write-Host "Length: $($en.Length)"
-Write-Host "Tail: $($en.Substring($i))"
--- _verify_resx.ps1
+++
@@ -1,9 +0,0 @@
-$vi = [System.IO.File]::ReadAllText('d:\BlazorWasmPortfolioGhAction\Resources\SharedResources.resx', [System.Text.Encoding]::UTF8)
-$en = [System.IO.File]::ReadAllText('d:\BlazorWasmPortfolioGhAction\Resources\SharedResources.en.resx', [System.Text.Encoding]::UTF8)
-foreach ($k in @('Common_Loading','Common_Searching','Common_Saving','Common_LoadingData','Common_LoadingAria','Nav_LoadingPage')) {
-    $viI = $vi.IndexOf($k + '"')
-    $enI = $en.IndexOf($k + '"')
-    Write-Host "=== $k ==="
-    if ($viI -ge 0) { Write-Host "VI: $($vi.Substring($viI, 80))" } else { Write-Host "VI: NOT FOUND" }
-    if ($enI -ge 0) { Write-Host "EN: $($en.Substring($enI, 80))" } else { Write-Host "EN: NOT FOUND" }
-}
